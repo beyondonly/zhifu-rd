c55543e0acabe1033000e0585d233c3480a73110
System Administrator <root@MacBook-Pro-3.local>
2016-07-03 23:09:23 +0800

    genxin

--- routes/index.js
+++ routes/index.js
@@ -145,45 +145,49 @@ function login(callback) {
 		}); 
 	},"utf-8")
 }
-// login(function(data,cookieupload){
-// 	var kk = 'q_c1=8fa71740180d4c11bfa3f55c4aa29c09|1467302654000|1467302654000; _xsrf=e3963da41a0754893a6f1878a39f6c60; l_cap_id="YWE4OThkOTU1ZmRmNDRhNTkzYTEyMTYzNjllN2FhZjY=|1467302654|07201c27cb05a583693a6ac6c5fb1b2a1b0d76f5"; cap_id="MmFhYjFkMTA2MmFiNDUzZGFjMjc4OWU4YjY0M2E3OTI=|1467302654|a76b86355bef9e6bc4259e74a0b576d2d69c0fb2"; n_c=1';
-// 	var querystring = require('querystring');
-// 	var data=querystring.stringify({password:"939593FCTLOVEDLX",remember_me:true,email:"825804189@qq.com",_xsrf:'e3963da41a0754893a6f1878a39f6c60',captcha:"n9vh"});
-// 	var opt = {  
-//         method: "POST",  
-//         host: "www.zhihu.com",  
-//         port: 443,  
-//         path: "/login/email",  
-//         headers: {
-// 			"User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
-//         	"Accept":'*/*',
-// 			'Accept-Encoding':'gzip, deflate, br',
-// 			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
-// 			'Cache-Control':'no-cache',
-//             'Content-Type':"application/x-www-form-urlencoded",
-//             "Connection":"keep-alive",
-//             "Content-Length": data.length,
-//             "Cookie":kk,
-//             'Host':"www.zhihu.com",
-// 			'Origin':"https://www.zhihu.com",
-// 			"X-Requested-With":"XMLHttpRequest",
-// 			"Pragma":"no-cache",
-// 			"Referer":"https://www.zhihu.com/"
-//         }  
-// 	};  
-// 	var req = https.request(opt, function (serverFeedback) { 
-// 			console.log(serverFeedback.statusCode) 
-//         serverFeedback.setEncoding('utf8');
-//         serverFeedback.on("data",function(cendata) {
-//         	var enddata = JSON.parse(cendata);
-//         	console.log(enddata)
-//         })
-//         return;
-//     }).on('error', function(e) {
-//       console.log("Got error: " + e.message);
-// 	});
-// 	req.write(data);
-// 	req.end();
-// })
+login(function(data,cookieupload){
+	var kk = '_za=4b343ebd-a751-493c-9e8a-6095ba11e4df; _xsrf=7890b609478a1d2bae8d90d25fdcbac8; __utmt=1; _zap=f0a74007-6994-4773-8c35-d3946ad419f8; q_c1=b183d9b42f024722b45d044f97125e05|1467558289000|1467558289000; l_cap_id="YjA2ZDU5OGViYzhkNDI0ZjkxMmNkMzk0ZTM4MDdkNzY=|1467558289|6e918e6fa82f34260d4dbc1787df8e636f391df6"; cap_id="MzYxNjlhMGQ0NzYzNGM1ZWIxN2RiZGJhYTFkODQyNGU=|1467558289|9722a4c479c6570fcec0151c6f362fb10a7b3eb9"; d_c0="ADBApkNmLAqPTuzTwOnGtAPtzkw3QSrQY9s=|1467558289"; login="NGY3ZTZiNTkzODAyNDVmMGI1NDE3ZmY2YWE0YzdjNmU=|1467558428|921d3f1e077edbcd92195e00136855cc5db7eb33"; a_t="2.0AABA7soqAAAXAAAAHLegVwAAQO7KKgAAADBApkNmLAoXAAAAYQJVTRy3oFcABYHEsuqZvk4X4lsUGrKEjUwXya10epAsxor1U1Y4Yj6zO36YJGTZOw=="; z_c0=Mi4wQUFCQTdzb3FBQUFBTUVDbVEyWXNDaGNBQUFCaEFsVk5ITGVnVndBRmdjU3k2cG0tVGhmaVd4UWFzb1NOVEJmSnJR|1467558428|5b85e319ea9bed7dc960845d395a974fba8cf8fa; n_c=1; __utma=51854390.635298240.1467558479.1467558479.1467558479.1; __utmb=51854390.2.10.1467558479; __utmc=51854390; __utmz=51854390.1467558479.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=51854390.100-1|2=registration_date=20140404=1^3=entry_date=20140404=1';
+	var querystring = require('querystring');
+	var data=querystring.stringify({password:"39593FCTLOVEDLX",remember_me:true,email:"825804189@qq.com",_xsrf:'7890b609478a1d2bae8d90d25fdcbac8',captcha:"n9vh"});
+	var opt = {  
+        method: "POST",  
+        host: "www.zhihu.com",  
+        port: 443,  
+        path: "/login/email",  
+        headers: {
+			"User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+        	"Accept":'*/*',
+			'Accept-Encoding':'gzip, deflate, br',
+			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
+			'Cache-Control':'no-cache',
+            'Content-Type':"application/x-www-form-urlencoded",
+            "Connection":"keep-alive",
+            "Content-Length": data.length,
+            "Cookie":kk,
+            'Host':"www.zhihu.com",
+			'Origin':"https://www.zhihu.com",
+			"X-Requested-With":"XMLHttpRequest",
+			"Pragma":"no-cache",
+			"Referer":"https://www.zhihu.com/"
+        }  
+	};  
+	var req = https.request(opt, function (serverFeedback) { 
+			console.log(serverFeedback.statusCode) 
+        serverFeedback.setEncoding('utf8');
+        serverFeedback.on("data",function(cendata) {
+        	var enddata = JSON.parse(cendata);
+        	console.log(enddata)
+        })
+        return;
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+	});
+	req.write(data);
+	req.end();
+})
+
+
+
+
 
 module.exports = router;
